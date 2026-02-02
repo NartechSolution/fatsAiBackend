35e94e0e011b27756123cc25795af4b0f55f0ff8
Eissanoor <eissaanoor@gmail.com>
2026-02-02 15:35:03 +0500

    refactor: enhance city routes by adding token verification middleware for improved security

--- routes/cityRoutes.js
+++ routes/cityRoutes.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const cityController = require('../controllers/cityController');
-const { verifyAdminToken } = require('../middleware/auth');
+const { verifyAdminToken, verifyToken } = require('../middleware/auth');
 
 // Create a new city - protected
-router.post('/', verifyAdminToken, cityController.createCity);
+router.post('/', verifyAdminToken, verifyToken, cityController.createCity);
 
 // Get all cities - protected
-router.get('/', verifyAdminToken, cityController.getAllCities);
+router.get('/', verifyAdminToken, verifyToken, cityController.getAllCities);
 
 // Get city by ID - protected
-router.get('/:id', verifyAdminToken, cityController.getCityById);
+router.get('/:id', verifyAdminToken, verifyToken, cityController.getCityById);
 
 // Update city - protected
-router.put('/:id', verifyAdminToken, cityController.updateCity);
+router.put('/:id', verifyAdminToken, verifyToken, cityController.updateCity);
 
 // Delete city - protected
-router.delete('/:id', verifyAdminToken, cityController.deleteCity);
+router.delete('/:id', verifyAdminToken, verifyToken, cityController.deleteCity);
 
 module.exports = router;
 
